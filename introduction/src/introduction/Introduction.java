0036b35f948f657190e7fbe78cf1fe8cd29170bf
amandadhkm <amandadhkm20@gmail.com>
2025-02-12 14:48:49 +0700

    Add files via upload

---
+++ introduction/src/introduction/Introduction.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package introduction;
+
+/**
+ *
+ * @author amandadhkm
+ */
+public class Introduction {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hallo World Amanda Devika Hakim 2307254 Saya Senangf Belajara Java");
+    }
+    
+}
